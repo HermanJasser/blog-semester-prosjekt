8d1c1e70985effde6ad619349674c7386dd3bce9
HermanJasser <herjas00989@stud.noroff.no>
2024-05-12 16:49:14 +0200

    liten update på sitemappet

--- js/utils.js
+++ js/utils.js
@@ -19,7 +19,11 @@
         });
     } else {
         //console.log ("NOT Logged in");
-        loggedInOut.innerHTML = `<a href="../account/login.html">Log inn</a>`;
+        if(window.location.pathname === "/index.html" || window.location.pathname === "/" || window.location.pathname === ""){
+        loggedInOut.innerHTML = `<a href="./account/login.html">Log inn</a>`;
+        } else{
+            loggedInOut.innerHTML = `<a href="../account/login.html">Log inn</a>`
+        }
     }
 }
 
